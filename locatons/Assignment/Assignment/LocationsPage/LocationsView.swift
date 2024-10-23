a1898093b18e88cdf01babc49948c11f5274399a
dundercodes <solute-bulletin-0a@icloud.com>
2024-10-23 09:42:50 +0200

    Add accessibility labels to LocationsView

--- locatons/Assignment/Assignment/LocationsPage/LocationsView.swift
+++ locatons/Assignment/Assignment/LocationsPage/LocationsView.swift
@@ -7,7 +7,7 @@ struct LocationsView: View {
   
   @ObservedObject var viewModel: LocationsViewModel
   
-  init(viewModel: LocationsViewModel = .init(), delegate: LocationsViewModelDelegate) {
+  init(viewModel: LocationsViewModel = .init(), delegate: LocationsViewModelDelegate?) {
     self.viewModel = viewModel
     self.viewModel.delegate = delegate
   }
@@ -39,7 +39,7 @@ struct LocationsView: View {
 // MARK: - Private
 private extension LocationsView {
   func existingLocationsSection(_ locations: [Location]) -> some View {
-    Section("Existing") {
+    Section("Known locations") {
       ForEach(locations.indices, id: \.self) { index in
         let location = locations[index]
         VStack(alignment: .leading) {
@@ -50,6 +50,10 @@ private extension LocationsView {
           Text(location.description)
             .font(.caption)
         }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(location.name ?? location.description)
+        .accessibilityAddTraits(.isButton)
+        .accessibilityRemoveTraits(.isHeader)
         .onTapGesture {
           viewModel.didSelectExistingLocation(location)
         }
@@ -58,10 +62,17 @@ private extension LocationsView {
   }
   
   func newLocationInputs(_ onTapGo: @escaping (Double?, Double?) -> Void) -> some View {
-    Section("New") {
-      TextField("Latitude", value: $customLatitude, format: .number)
-      TextField("Longitude", value: $customLongitude, format: .number)
-      
+    Section("New coordinates") {
+      let latitudeKey: LocalizedStringKey = "Latitude"
+      TextField(latitudeKey, value: $customLatitude, format: .number)
+        .accessibilityLabel(latitudeKey)
+        .accessibilityValue(customLatitude?.description ?? "")
+
+      let longitudeKey: LocalizedStringKey = "Longitude"
+      TextField(longitudeKey, value: $customLongitude, format: .number)
+        .accessibilityLabel(longitudeKey)
+        .accessibilityValue(customLongitude?.description ?? "")
+
       Button("Go") {
         onTapGo(customLatitude, customLongitude)
       }
