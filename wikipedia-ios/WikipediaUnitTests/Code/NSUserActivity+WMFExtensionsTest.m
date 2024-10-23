90194f24f18d83306ab82ce2431b59e394e404f4
dundercodes <solute-bulletin-0a@icloud.com>
2024-10-23 09:42:50 +0200

    Add test for coordinates deeplink

--- wikipedia-ios/WikipediaUnitTests/Code/NSUserActivity+WMFExtensionsTest.m
+++ wikipedia-ios/WikipediaUnitTests/Code/NSUserActivity+WMFExtensionsTest.m
@@ -52,5 +52,22 @@
                           @"https://en.wikipedia.org/w/index.php?search=dog&title=Special:Search&fulltext=1");
 }
 
+- (void)testPlaces_givenCoordinates_shouldUpdateActivityWithCoordinates {
+  // Arrange
+  NSNumber *latitude = @52.3547498;
+  NSNumber *longitude = @4.8339215;
+  NSString *urlString = [NSString stringWithFormat:@"wikipedia://places/coordinates?lat=%@&long=%@", latitude, longitude];
+  NSURL *url = [NSURL URLWithString:urlString];
+  
+  // Act
+  NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+  NSNumber *lat = activity.userInfo[@"WMFPlacesCoordinatesLatitude"];
+  NSNumber *lon = activity.userInfo[@"WMFPlacesCoordinatesLongitude"];
+
+  // Assert
+  XCTAssert([lat isEqualToNumber:@52.3547498]);
+  XCTAssert([lon isEqualToNumber:@4.8339215]);
+}
+
 @end
 
